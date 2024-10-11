5ab02246fde8ab79068872a1abbf1c316f1e8471
Vladimir Skach <vladimir.skach@gmail.com>
2024-10-11 13:45:44 +0200

    nette extension files

---
+++ src/Extension/Nette/ImokutrExtension.php
@@ -0,0 +1,116 @@
+<?php
+namespace SkachCz\Imokutr3\Extension\Nette;
+
+use SkachCz\Imokutr3\Imokutr;
+use SkachCz\Imokutr3\Config;
+
+use SkachCz\Imokutr3\Exception\ImokutrNetteMissingExtension;
+
+use SkachCz\Imokutr3\Extension\Nette\ImokutrFilters;
+use SkachCz\Imokutr3\Extension\Nette\ImokutrMacros;
+
+use Nette\DI\Definitions\Definition;
+
+use Nette\Schema\Schema;
+use Nette\Schema\Expect;
+
+use Nette\DI\CompilerExtension;
+use Nette\PhpGenerator\ClassType;
+use Nette\Utils\Validators;
+
+if (!class_exists('Nette\DI\CompilerExtension')) {
+    throw new ImokutrNetteMissingExtension();
+}
+
+/**
+ * Imokutr Nette extension (for Nette 2.4)
+ *
+ * @package SkachCz\Imokutr\Nette
+ * @author Vladimir Skach
+ */
+final class ImokutrExtension extends CompilerExtension
+{
+    /**
+     * @return Config
+     */
+    public function getConfig() {
+        return $this->config;
+    }
+
+    /**
+     * @return void
+     */
+    public function loadConfiguration()
+    {
+        $builder = $this->getContainerBuilder();
+
+        //imokutr config provider:
+        $builder->addDefinition($this->prefix('imokutrProvider'))
+            ->setFactory(Imokutr::class, [$this->config]);
+        /*
+        $builder->addDefinition($this->prefix('imokutrProvider'))
+            ->setFactory(Imokutr::class, [$this->config]);
+        */
+    }
+
+    /**
+     * @return void
+     */
+    public function beforeCompile()
+    {
+        $builder = $this->getContainerBuilder();
+
+        $methodExists = method_exists($builder->getDefinition('latte.latteFactory'), 'getResultDefinition');
+
+        if ($methodExists) {
+            /* nette 3.0: */
+            $builder->getDefinition('latte.latteFactory')
+                ->getResultDefinition()->addSetup('addProvider', ['imokutrProvider', $this->prefix('@imokutrProvider')]);
+        } else {
+            $builder->getDefinition('latte.latteFactory')
+                ->addSetup('addProvider', ['imokutrProvider', $this->prefix('@imokutrProvider')]);
+        }
+
+        if ($builder->hasDefinition('nette.latteFactory')) {
+
+            $factory = $builder->getDefinition('nette.latteFactory');
+
+            // filter registration:
+            $filters = new ImokutrFilters($this->config);
+
+
+            if ($methodExists) {
+                /* nette 3.0 */
+                $factory->getResultDefinition()->addSetup('addFilter', ['imoUrl', [$filters, 'imoUrl']]);
+            } else {
+                $factory->addSetup('addFilter', ['imoUrl', [$filters, 'imoUrl']]);
+            }
+
+            // macro registration:
+            $method = '?->onCompile[] = function($engine)  {
+                SkachCz\Imokutr\Nette\ImokutrMacros::install($engine->getCompiler());
+            }';
+
+            if ($methodExists) {
+                /* nette 3.0 */
+                $factory->getResultDefinition()->addSetup($method, ['@self']);
+            } else {
+                $factory->addSetup($method, ['@self']);
+            }
+
+        }
+    }
+
+    public function getConfigSchema(): Schema
+    {
+        return Expect::structure([
+			'originalRootPath' => Expect::string(),
+            'thumbsRootPath' => Expect::string(),
+            'thumbsRootRelativePath' => Expect::string(),
+            'defaultImageRelativePath' => Expect::string()->default('default.png'),
+            'qualityJpeg' => Expect::int()->default(75),
+            'qualityPng' => Expect::int()->default(6),
+		]);
+    }
+
+}
---
+++ src/Extension/Nette/ImokutrFilters.php
@@ -0,0 +1,53 @@
+<?php
+namespace SkachCz\Imokutr3\Extension\Nette;
+
+use SkachCz\Imokutr3\Config;
+use SkachCz\Imokutr3\Image;
+use SkachCz\Imokutr3\Thumbnail;
+
+/**
+ * @package SkachCz\Imokutr\Nette
+ * @author Vladimir Skach
+ */
+class ImokutrFilters {
+
+    /** @var Config */
+    public $config;
+
+    public function __construct(Config $config)
+	{
+        $this->config = $config;
+    }
+
+    /**
+     * @return string
+     */
+    public function imoUrl(string $path, int $width, int $height, string $fixedDimension = 'w', int $cropType = Image::CROP_CENTER, bool $force = false) {
+
+        switch($fixedDimension) {
+
+            case 'c':
+                $fixed = Image::DIM_CROP;
+                break;
+
+            case 'h':
+                $fixed = Image::DIM_HEIGHT;
+                break;
+
+            default:
+                $fixed = Image::DIM_WIDTH;
+        }
+
+        $image = new Image($this->config->originalRootPath, $path, $this->config->defaultImageRelativePath);
+        $thumbnail = new Thumbnail($this->config, $image);
+
+        $thumbnail->setResize($width, $height, $fixed, $cropType);
+
+        $thumbnail->processImage($force);
+
+        return $thumbnail->getThumbnailUrl();
+
+    }
+
+
+}
